457937ca973cbf42d3d47b07056eee050ccc4e8d
Samuel Ng <eclmist@yahoo.com>
2018-05-25 20:06:52 +0800

    added dynamic navbar part 1

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="navbar is-transparent is-fixed-top" role="navigation" aria-label="main navigation">
+    <nav v-bind:class="{ top: navbarPos + 'px' }" class="navbar is-transparent is-fixed-top">
       <div class="navbar-brand">
         <a class="navbar-item" v-on:click="loginModalActive = true">
           <img src="https://i.stack.imgur.com/HQwHI.jpg">
@@ -36,15 +36,37 @@ import Login from './Login'
 export default {
   data () {
     return {
+      previousFrameWindowYOffset: 0,
+      scrollDelta: 0,
+      navbarPos: 0,
       loginModalActive: false,
       burgerEaten: false
     }
   },
+  created () {
+    window.addEventListener('scroll', this.scrollEvent)
+  },
+  destroyed () {
+    window.removeEventListener('scroll', this.scrollEvent)
+  },
   components: {
     'login-modal': Login
+  },
+  methods: {
+    scrollEvent () {
+      this.scrollDelta = window.pageYOffset - this.previousFrameWindowYOffset
+
+      this.navbarPos = Math.min(0, Math.max(-51, (this.navbarPos - this.scrollDelta)))
+
+      // this.navbarPos = 10000
+
+      // dont touch
+      this.previousFrameWindowYOffset = window.pageYOffset
+    }
   }
 }
 </script>
+
 
 <style scoped>
 
