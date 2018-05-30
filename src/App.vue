1fea49462dde10a0055aac0e4726f8023e134961
Samuel Ng <eclmist@yahoo.com>
2018-05-30 13:55:19 +0800

    removed background animation for performance reasons

--- src/App.vue
+++ src/App.vue
@@ -40,32 +40,16 @@ html, body
   height: 100%;
 }
 
-html
+.background
 {
-  /* background: linear-gradient(141deg, #30b9c9 0%, #2c7ee8 100%); */
-  /* background-image: url('/static/img/background/welpbg.jpeg') !important */
-  
-  /* background: linear-gradient(205deg, #4f65f8, #c583e7, #c4e783);
-  background-size: 300% 300%; */
-  background: linear-gradient(0deg, #6ecacd, #8390e7, #c183e7, #e78383, #c183e7,
-  #8390e7, #6ecacd);
-  background-size: 500% 500%;
-  -webkit-animation: background 150s ease infinite;
-  animation: background 150s ease infinite;
-
-  /* mix-blend-mode:hard-light; */
-}
-
-@-webkit-keyframes background {
-  0%{background-position:0% 0%}
-  /* 50%{background-position:0% 100%} */
-  100%{background-position:0% 500%}
-}
-
-@keyframes background { 
-  0%{background-position:0% 0%}
-  /* 50%{background-position:0% 100%} */
-  100%{background-position:0% 500%}
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: -100;
+  background: linear-gradient(205deg, #4f65f8, #c583e7);
+  background-size: 100% 100%;
 }
 
 .main-background
