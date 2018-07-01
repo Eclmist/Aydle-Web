dccfad3a528f27caf3fa63b6b2c8ec8ae9d05452
Samuel Ng <eclmist@yahoo.com>
2018-07-02 03:07:27 +0800

    added on peerupdate functions

--- src/js/ClientSocket.js
+++ src/js/ClientSocket.js
@@ -33,20 +33,8 @@ export default class ClientSocket {
       // socket = io('http://localhost:2000/')
       socket.on('onConnected', callback)
 
-      // When a new user has joined
-      // Use this to update the client's player list
-      socket.on('notifyJoin', newPlayerObject => {
-        callbacks.onOtherPlayerJoin(newPlayerObject)
-      })
-
-      // When the user successfully connects but have not set his name yet
-      // User gets the latest updated room object
-      socket.on('onJoin', roomObject => {
-        callbacks.onJoin(roomObject, this.socket)
-      })
-
-      socket.on('onPlayerUpdate', playerObject => {
-        callbacks.onPlayerUpdate(playerObject)
+      socket.on('onPeerUpdate', playerObject => {
+        callbacks.onPeerUpdate(playerObject)
       })
 
       socket.on('onJoinFail', () => {
