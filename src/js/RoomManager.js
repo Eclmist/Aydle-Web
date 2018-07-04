0bd01b9cf96f8db98ac0ba17a5a3de7333205713
Samuel Ng <eclmist@yahoo.com>
2018-07-04 14:06:33 +0800

    joinroom fix

--- src/js/RoomManager.js
+++ src/js/RoomManager.js
@@ -8,23 +8,22 @@ export default class RoomManager {
 
     RoomManager.prototype.joinRoom = function (code, playerID) {
       clientSocket.initSocketConnection(
-        LOBBY_SOCKET, AYDLE_LOBBY_API,
-        function () {
+        LOBBY_SOCKET, AYDLE_LOBBY_API, () => {
           clientSocket.getSocket(LOBBY_SOCKET).emit('requestJoin', code, playerID,
-            function (roomObject) {
-              clientSocket.getCallbacks().onJoin(roomObject, clientSocket.getSocket(LOBBY_SOCKET))
-            },
-            clientSocket.getCallbacks().onFailure())
-        })
+            clientSocket.getCallbacks().onJoin
+            // clientSocket.getCallbacks().onFailure)
+          )
+        }
+      )
     }
 
     RoomManager.prototype.hostRoom = (playerID, lobbyName) => {
-      clientSocket.initSocketConnection(LOBBY_SOCKET, AYDLE_LOBBY_API,
-        function () {
-          clientSocket.getSocket(LOBBY_SOCKET).emit('requestHost', playerID, lobbyName,
-            function (roomObject) {
-              clientSocket.getCallbacks().onJoin(roomObject)
-            })
+      clientSocket.initSocketConnection(
+        LOBBY_SOCKET, AYDLE_LOBBY_API, () => {
+          clientSocket.getSocket(LOBBY_SOCKET).emit('requestHost',
+            playerID,
+            lobbyName,
+            clientSocket.getCallbacks().onJoin)
         })
     }
 
